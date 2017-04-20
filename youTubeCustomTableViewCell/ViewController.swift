edbe2104870f62a6cc62c20bb81d89beeb1a8348
twumasid <twumasi.d@gmail.com>
2017-04-20 23:14:59 +0200

    Initial Commit

---
+++ youTubeCustomTableViewCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youTubeCustomTableViewCell
+//
+//  Created by daniel on 20.04.17.
+//  Copyright © 2017 daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
